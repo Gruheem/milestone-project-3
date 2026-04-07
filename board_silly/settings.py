7ef4e8b4a048fbe3cdf3a083797a56b88466536d
Gruheem <grahamsilcock91@gmail.com>
2026-04-07 14:38:03 +0100

    Add Allowed Hosts

--- board_silly/settings.py
+++ board_silly/settings.py
@@ -29,7 +29,10 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
