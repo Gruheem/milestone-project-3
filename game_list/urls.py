79bf3d58da6f5d1da948bc5f84ec0733b2b9a9ca
Gruheem <grahamsilcock91@gmail.com>
2026-04-09 11:36:57 +0100

    Add base.html Template

--- game_list/urls.py
+++ game_list/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.GameList.as_view(), name='game_list'),
+    path('', views.GameList.as_view(), name='home'),
 ]
--- game_list/views.py
+++ game_list/views.py
@@ -5,4 +5,5 @@ from .models import BoardGame
 # Create your views here.
 class GameList(generic.ListView):
     queryset = BoardGame.objects.filter(status=1)
-    template_name = 'boardgame_list.html'
+    template_name = "index.html"
+    paginate_by = 6
