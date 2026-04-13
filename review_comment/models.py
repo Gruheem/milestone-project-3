0cd55832a40aca2df5f935846bcff7c38515efd9
Gruheem <grahamsilcock91@gmail.com>
2026-04-13 12:04:27 +0100

    Create Review Model

--- review_comment/models.py
+++ review_comment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from game_list.models import BoardGame
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Review(models.Model):
+    boardgame = models.ForeignKey(BoardGame, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
