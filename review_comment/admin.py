5bade587f6d383f605dbaa019befa2f56abfd5d4
Gruheem <grahamsilcock91@gmail.com>
2026-04-13 12:49:00 +0100

    Register Comment in admin.py

--- review_comment/admin.py
+++ review_comment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -10,5 +10,14 @@ class ReviewAdmin(SummernoteModelAdmin):
     list_display = ('title', 'boardgame', 'author', 'rating', 'created_at')
     search_fields = ['title', 'content']
     list_filter = ('rating', 'created_at',)
+    prepopulated_fields = {}
+    summernote_fields = ('content',)
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('review', 'author', 'created_at')
+    search_fields = ['content', 'author']
+    list_filter = ('created_at',)
     prepopulated_fields = {}
     summernote_fields = ('content',)
