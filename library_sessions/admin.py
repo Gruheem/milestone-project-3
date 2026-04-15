1a8b75c87a387031a1bcc3cfbc2276e9c24e978d
Gruheem <grahamsilcock91@gmail.com>
2026-04-15 13:04:31 +0100

    Add LibraryEntry Admin Pannel

--- library_sessions/admin.py
+++ library_sessions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import LibraryEntry
 
 # Register your models here.
+@admin.register(LibraryEntry)
+class LibraryEntryAdmin(admin.ModelAdmin):
+
+    list_display = ('user', 'boardgame', 'status', 'added_at')
+    search_fields = ['user__username', 'boardgame__name']
+    list_filter = ('status', 'added_at')
